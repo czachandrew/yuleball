996e9a9b2b533e063efce013e6954eac7a0837ff
Andrew Czachowski <andrewczachowski@Andrews-iMac.local>
2022-01-15 13:36:42 -0600

    quize page

---
+++ src/views/Quiz.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="mx-auto my-auto">
+    <h5 class="text-3xl potterize">This areas is strictly out of bounds</h5>
+    <p>credentials lack property authority</p>
+  </div>
+</template>
+<script lang="ts" setup></script>
